c2aac9354ce118976b6ca84988f8074d81b8f6b3
tanmayaarora <tanmaya.arora@chd.trantorinc.com>
2023-03-18 15:29:00 +0530

    Added dropdown to change bulb colour and inline-block css class

--- src/Components/Bulb/Bulb.js
+++ src/Components/Bulb/Bulb.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import BulbBase from "../BulbBase/BulbBase";
 import "./Bulb.css";
 
-const Bulb = ({id}) => {
+const Bulb = ({ id }) => {
     const [bulbOn, setBulbOn] = useState(false);
     const [bulbClass, setBulbClass] = useState("");
-    
+
     useEffect(() => {
-        if(bulbOn) {
+        if (bulbOn) {
             setBulbClass("bg-blue");
         }
         else {
@@ -15,13 +15,30 @@ const Bulb = ({id}) => {
         }
     }, [bulbOn]);
     return (
-        <div
-        className={`bulb ${bulbClass}`}
-        id={id}
-        onClick={() => setBulbOn((state) => !state)}
-        >
-            <BulbBase/>
-        </div>
+        <>
+            <div
+                className={`bulb d-inline-block ${bulbClass}`}
+                id={id}
+                onClick={() => setBulbOn((state) => !state)}
+            >
+                <BulbBase />
+            </div>
+            {bulbOn && <div className="d-inline-block">
+                <select
+                id="colordropdown"
+                name="colordropdown"
+                onChange={(event) => setBulbClass(event.target.value)}
+                defaultValue="bg-blue"
+                >
+                    <option value="bg-aqua">Aqua</option>
+                    <option value="bg-blue">Blue</option>
+                    <option value="bg-green">Green</option>
+                    <option value="bg-salmon">Salmon</option>
+                    <option value="bg-seagreen">Seagreen</option>
+                    <option value="bg-yellow">Yellow</option>
+                </select>
+            </div>}
+        </>
     );
 }
 
